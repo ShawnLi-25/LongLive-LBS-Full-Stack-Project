99ac0c3bee9de109790a25f2bfea777b1fde7ef9
xiangl14 <xiangl14@illinois.edu>
2020-04-01 15:27:37 -0500

    refactor rest api

--- backend/express/config/config.js
+++ backend/express/config/config.js
@@ -1,2 +1,3 @@
 exports.RADIUS = 200; // radius for heatmap
-exports.EVENTNUM = 3; // show event numbers for click
+exports.EVENTNUM = 10; // show event numbers for click
+exports.HEATMAPLIMIT = 10; // limit for return event number for hitmap
