cd46d9b5e8ff6c27b11cc29b529a886f9e8d7d56
ruiliu7 <ruiliu7@illinois.edu>
2020-04-05 18:35:14 -0500

    update config with method name

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 exports.LOGIN = "http://ec2-3-17-39-236.us-east-2.compute.amazonaws.com:3000/login";
-exports.DATA = "http://ec2-3-17-39-236.us-east-2.compute.amazonaws.com"
+exports.DATA = "http://ec2-3-17-39-236.us-east-2.compute.amazonaws.com:3000/getNearbyEvents"
